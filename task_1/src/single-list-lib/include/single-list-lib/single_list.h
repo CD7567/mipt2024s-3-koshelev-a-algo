024752addab2b30ddac811f96774853883f4700e
CD7567 <alexander.y.koshelev@gmail.com>
2024-02-19 13:05:04 +0300

    Added single_list.h

---
+++ task_1/src/single-list-lib/include/single-list-lib/single_list.h
@@ -0,0 +1,188 @@
+#ifndef TEMPLATES_LIST_2024_02_12
+#define TEMPLATES_LIST_2024_02_12
+
+#include <cstddef>
+
+namespace lab618
+{
+
+template <class T> class CSingleLinkedList
+{
+  private:
+    struct leaf
+    {
+        // Данные
+        T data;
+        // Указатель на следующий лист списка
+        leaf *pNext;
+        leaf(T &_data, leaf *_pNext) : data(_data), pNext(_pNext)
+        {
+        }
+    };
+
+  public:
+    class CIterator
+    {
+      public:
+        CIterator() : m_pCurrent(nullptr), m_pBegin(nullptr)
+        {
+        }
+
+        CIterator(leaf *p)
+        {
+        }
+
+        CIterator(const CIterator &src)
+        {
+        }
+
+        ~CIterator()
+        {
+        }
+
+        CIterator &operator=(const CIterator &src)
+        {
+        }
+
+        bool operator!=(const CIterator &it) const
+        {
+            return true;
+        }
+
+        void operator++()
+        {
+        }
+
+        T &getData()
+        {
+            return T();
+        }
+
+        T &operator*()
+        {
+            T tmp;
+            return tmp;
+        }
+
+        leaf *getLeaf()
+        {
+            return 0;
+        }
+
+        void setLeaf(leaf *p)
+        {
+        }
+
+        void setLeafPreBegin(leaf *p)
+        {
+        }
+
+        bool isValid()
+        {
+            return false;
+        }
+
+      private:
+        // храним голову списка, если мы находимся перед началом
+        leaf *m_pBegin;
+        // храним текущее положение
+        leaf *m_pCurrent;
+    };
+
+  public:
+    CSingleLinkedList() : m_pBegin(nullptr), m_pEnd(nullptr)
+    {
+    }
+
+    virtual ~CSingleLinkedList()
+    {
+        for (leaf *it = m_pBegin; it != nullptr;)
+        {
+            leaf *to_delete = it;
+
+            it = it->pNext;
+            delete to_delete;
+        }
+    }
+
+    void pushBack(T &data)
+    {
+        leaf *p_newNode = new leaf(data, nullptr);
+
+        if (m_pEnd != nullptr)
+        {
+            m_pEnd->pNext = p_newNode;
+            m_pEnd = p_newNode;
+        }
+        else
+        {
+            m_pBegin = m_pEnd = p_newNode;
+        }
+    }
+
+    void pushFront(T &data)
+    {
+        leaf *p_newNode = new leaf(data, m_pBegin);
+
+        if (m_pBegin == nullptr)
+        {
+            m_pBegin = m_pEnd = p_newNode;
+        }
+        else
+        {
+            m_pBegin = p_newNode;
+        }
+    }
+
+    T popFront()
+    {
+        T tmp = m_pBegin->data;
+        leaf *p_toRemove = m_pBegin;
+
+        m_pBegin = m_pBegin->pNext;
+
+        delete p_toRemove;
+        return tmp;
+    }
+
+    // изменяет состояние итератора. выставляет предыдущую позицию.
+    void erase(CIterator &it)
+    {
+    }
+
+    int getSize()
+    {
+        int size = 0;
+
+        for (leaf *it = m_pBegin; it != nullptr; ++size, it = it->pNext)
+        {
+        }
+
+        return size;
+    }
+
+    void clear()
+    {
+        for (leaf *it = m_pBegin; it != nullptr;)
+        {
+            leaf *to_delete = it;
+
+            it = it->pNext;
+            delete to_delete;
+        }
+
+        m_pBegin = m_pEnd = nullptr;
+    }
+
+    CIterator begin()
+    {
+        return CIterator();
+    }
+
+  private:
+    // храним голову и хвост списка
+    leaf *m_pBegin, *m_pEnd;
+};
+}; // namespace lab618
+
+#endif // #ifndef TEMPLATES_LIST_2024_02_12
