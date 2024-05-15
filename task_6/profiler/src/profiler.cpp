e6a432033a592ca28d4932a15daca286f8c0def0
CD7567 <alexander.y.koshelev@gmail.com>
2024-05-15 17:06:54 +0300

    Add profiler utils

--- task_6/profiler/src/profiler.cpp
+++ task_6/profiler/src/profiler.cpp
@@ -1,4 +1,19 @@
+#include <chrono>
+#include <format>
 #include <iostream>
+
+#define TIME(funcCall, ChronoType, storage)                                                                            \
+    {                                                                                                                  \
+        auto start = std::chrono::high_resolution_clock::now();                                                        \
+        funcCall;                                                                                                      \
+        auto end = std::chrono::high_resolution_clock::now();                                                          \
+        storage = std::chrono::duration_cast<ChronoType>(end - start).count();                                         \
+    }
+
+template <typename... Args> std::string dyn_format(std::string_view format_str, Args &&...args)
+{
+    return std::vformat(format_str, std::make_format_args(args...));
+}
 
 int main(int argc, char **argv)
 {
